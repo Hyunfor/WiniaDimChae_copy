068ff7c60b6b2b430749ecb087b3be23b17f1d8f
Hyunfor <navel5148@gmail.com>
2022-03-30 12:14:20 +0900

    진행중

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 $(document).ready(function(){
 
-    $(".slider-main > .slider-list").slick({})
+  $('.slider-main > .slider-wrap').slick({
+    dots: true,
+    });
+  //   $(".slider-main > .slider-wrap").slick({})
 
-    $(".left-btn").click(function(){
-        $(".slider-main > .slider-list").slick("slickPrev")
-      })
-      $(".right-btn").click(function(){
-        $(".slider-main > .slider-list").slick("slickNext")
-      })
+  //   $(".left-btn").click(function(){
+  //       $(".slider-main > .slider-wrap").slick("slickPrev")
+  //     })
+  //     $(".right-btn").click(function(){
+  //       $(".slider-main > .slider-wrap").slick("slickNext")
+  //     })
 
-    $(".dot").click(function(){
-        let num = $(this).index()
-        $(".slider-wrap > .slider").slick("slickGoTo",num)
-      })
+  //   $(".dot").click(function(){
+  //       let num = $(this).index()
+  //       $(".slider-wrap > .slider").slick("slickGoTo",num)
+  //     })
 
-      $(".play").click(function(){
-        $(".play").removeClass("active")
-        $(".pause").addClass("active")
-        $(".slider-main > .slider-list").slick("slickPlay")
-      })
-      $(".pause").click(function(){
-        $(".play").addClass("active")
-        $(".pause").removeClass("active")
-        $(".slider-main > .slider-list").slick("slickPause")
-      })
+  //     $(".play").click(function(){
+  //       $(".play").removeClass("active")
+  //       $(".pause").addClass("active")
+  //       $(".slider-main > .slider-wrap").slick("slickPlay")
+  //     })
+  //     $(".pause").click(function(){
+  //       $(".play").addClass("active")
+  //       $(".pause").removeClass("active")
+  //       $(".slider-main > .slider-wrap").slick("slickPause")
+  //     })
 
-      $('.slider-main > .slider-list').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-        $(".prograss").removeClass("active")
-  });
+  //     $('.slider-main > .slider-list').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  //       $(".prograss").removeClass("active")
+  // });
     
-    $('.slider-main > .slider-list').on('afterChange', function(event, slick, currentSlide){
-        $(".prograss").addClass("active")
-  });
+  //   $('.slider-main > .slider-list').on('afterChange', function(event, slick, currentSlide){
+  //       $(".prograss").addClass("active")
+  // });
 
 
 
